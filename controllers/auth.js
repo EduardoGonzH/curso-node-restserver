4470a593ea6e89071ad626192d0c92a0585edcc1
Eduardo <lalolai19@gmail.com>
2022-02-23 12:56:41 -0600

    Google singin add

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,25 +1,28 @@
-const bcryptjs = require("bcryptjs");
 const { response } = require("express");
-const { generarJWT } = require("../helpers/generar-jwt");
+const bcryptjs = require("bcryptjs");
+
 const Usuario = require('../models/usuario');
 
+const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
-const login = async(req, res = response) => {
+
+const login = async (req, res = response) => {
 
     const { correo, password } = req.body;
 
     try {
 
         //Verificar si el email existe
-        const usuario = await Usuario.findOne({correo});
-        if(!usuario){
+        const usuario = await Usuario.findOne({ correo });
+        if (!usuario) {
             return res.status(400).json({
                 msg: 'Usuario/Contraseña no son correctos - correo'
             })
         }
 
         //Si el usuario esta activo
-        if(!usuario.estado){
+        if (!usuario.estado) {
             return res.status(400).json({
                 msg: 'Usuario/Contraseña no son correctos - estado: false'
             })
@@ -28,7 +31,7 @@ const login = async(req, res = response) => {
 
         //Verificar contraseña
         const validPassword = bcryptjs.compareSync(password, usuario.password);
-        if(!validPassword){
+        if (!validPassword) {
             return res.status(400).json({
                 msg: 'Usuario/Contraseña no son correctos - password'
             })
@@ -55,6 +58,58 @@ const login = async(req, res = response) => {
 
 }
 
+const googleSingIn = async (req, res = response) => {
+
+    const { id_token } = req.body;
+
+    try {
+
+        const { correo, nombre, img} = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({ correo });
+
+        if (!usuario) {
+            //tengo q crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        // Si el usuario en bd es false
+        if (!usuario.estado) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        //generar JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        })
+
+    }
+
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSingIn
 }
